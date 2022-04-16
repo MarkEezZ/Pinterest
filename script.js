13eb85c38e9a26934867735d43d8bd2d9b537c41
OpolkoAnton <opo-anton@yandex.ru>
2022-04-16 15:37:56 +0300

    первоначальный вариант, еще буду стилизовать

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
-
-
+let button = document.getElementById("shevron");
+let windowBlock = document.getElementById('second');
+console.log(windowBlock);
+button.addEventListener('click', test);
+function test() {
+    windowBlock.classList.toggle('newStyle')
+}
