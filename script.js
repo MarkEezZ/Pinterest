06bdf3052a6c50ae37b4bb18ce6ce6a25bd0c79e
OpolkoAnton <opo-anton@yandex.ru>
2022-04-20 19:06:49 +0300

    more changes

--- script.js
+++ script.js
@@ -1,7 +1,49 @@
+//открывайет меню
+
+
 let button = document.getElementById("shevron");
 let windowBlock = document.getElementById('second');
 console.log(windowBlock);
 button.addEventListener('click', test);
+
 function test() {
-    windowBlock.classList.toggle('newStyle')
+    windowBlock.classList.toggle('newStyle');
+    button.classList.toggle('black');
+    console.log(event)
 }
+
+
+//модальное окно сделать!!!!!
+ 
+// function myFunction() {
+//     document.getElementById("second").classList.toggle("newStyle");
+// }
+
+// window.onclick = function (event) {
+//     if (!event.target.matches('.shevron')) {
+//         let dropdowns = document.getElementsByClassName("second");
+//         if (dropdowns.classList.contains('newStyle')) {
+//             dropdowns.classList.remove('newStyle');
+//         }
+//     }
+// }
+
+// console.log(event); 
+
+
+// document.getElementById("myDropdown").onblur = function() {
+//     myFunction();
+// }
+
+
+
+
+
+
+
+
+
+
+// poisk.onclick = function() {
+//     alert('AAAAAAAA');
+//   };
